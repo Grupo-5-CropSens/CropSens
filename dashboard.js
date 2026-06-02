0582acabf82e020a8bab8770efd18e37e78cdd57
AnaRomero08 <ana.romero@sptech.school>
2026-06-02 10:50:56 -0300

    Subindo o bob ia e rota de cadastro e login

---
+++ dashboard.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+
+var dashboardController = require("../controllers/dashboardController");
+
+router.get("/dados/:idPlantacao", function (req, res) {
+    dashboardController.buscarDados(req, res);
+});
+
+module.exports = router;
